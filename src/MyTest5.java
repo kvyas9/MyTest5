34c27e9ae38dbfca4248059e59a237c4b0777490
kvyas9 <nouser@noemail.com>
2014-11-14 10:26:50 +0530

    Initial Draft

---
+++ src/MyTest5.java
@@ -0,0 +1,12 @@
+//This is test file...
+
+public class MyTest5(){
+    
+    /**
+    * Main method
+    * @param args 
+    */
+    public void readDataFromFile(){
+        //Wow it is working...
+    }
+}
---
+++ src/Test5.java
@@ -0,0 +1,18 @@
+/**
+ * Main class
+ */
+public class Test5 {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {    	
+		System.out.println("Hello world, Hi...");
+    }
+    public void push(Object o){
+        //Pushing Hard..
+    }
+    public Object pop(){
+        //Also pulling..
+    }
+}
